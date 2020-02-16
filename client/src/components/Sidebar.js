e58b004710f44781085a1083f39f3ca61e6918d3
cg021 <christiang021@gmail.com>
2020-02-15 19:00:34 -0500

    integrated 3box chatbox for group messaging

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -9,10 +9,14 @@ import GroupIcon from '@material-ui/icons/Group';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import CreateIcon from '@material-ui/icons/Create';
 import CreateNewGroupModal from './CreateNewGroupModal';
+import ChatBox from '3box-chatbox-react';
+import { Route } from "react-router-dom";
+import Box from "3box";
 
 export default class Sidebar extends Component {
 	state = {
-		isCreateGroupModalOpen: true
+		isCreateGroupModalOpen: true,
+		needToAWeb3Browser: false
 	};
 	doSomething = () => {
 		this.setState({ isCreateGroupModalOpen: true });
@@ -20,6 +24,30 @@ export default class Sidebar extends Component {
 	onClose = () => {
 		this.setState({ isCreateGroupModalOpen: false });
 	};
+
+	async getAddressFromMetaMask() {
+		if (typeof window.ethereum == "undefined") {
+		  this.setState({ needToAWeb3Browser: true });
+		} else {
+		  window.ethereum.autoRefreshOnNetworkChange = false;
+		  const accounts = await window.ethereum.enable();
+		  this.setState({ accounts });
+		}
+	  }
+
+	async componentDidMount() {
+		await this.getAddressFromMetaMask();
+		if (this.state.accounts) {
+		  const box = await Box.openBox(this.state.accounts[0], window.ethereum);
+		  const space = await box.openSpace("test");
+		  this.setState({ box });
+		  this.setState({ space });
+		  await box.syncDone;
+		  await space.syncDone;
+		  console.log("3Box synced");
+		}
+	}
+
 	//closeModal, showModal, action, message, title, disableBackdropClick
 	render() {
 		const { isCreateGroupModalOpen } = this.state;
@@ -30,7 +58,7 @@ export default class Sidebar extends Component {
 					{[
 						{ text: 'Create New Groups', icon: <GroupIcon /> },
 						{ text: 'Create Vote', icon: <AddCircleIcon /> },
-						{ text: 'Vote', icon: <CreateIcon /> }
+						{ text: 'Vote', icon: <CreateIcon /> },
 					].map((item, index) => (
 						<ListItem onClick={this.doSomething} button key={item.text}>
 							<ListItemIcon>{item.icon}</ListItemIcon>
@@ -38,6 +66,17 @@ export default class Sidebar extends Component {
 						</ListItem>
 					))}
 				</List>
+				<Route>
+					{this.state.box && <ChatBox
+					spaceName="testingnov"
+					threadName="testingnovthread"
+					box={this.state.box}
+					currentUserAddr={this.state.accounts[0]}
+					mute={false}
+					popupChat
+					showEmoji
+					colorTheme="#181F21"/>}
+				</Route>
 			</Wrapper>
 		);
 	}
