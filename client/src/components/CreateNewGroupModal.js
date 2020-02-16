211769818e0b66a89fccfb634801612ddb023b36
cg021 <christiang021@gmail.com>
2020-02-15 20:31:04 -0500

    adding members to group through modal + group name field

--- client/src/components/CreateNewGroupModal.js
+++ client/src/components/CreateNewGroupModal.js
@@ -6,18 +6,72 @@ import DialogContent from '@material-ui/core/DialogContent';
 import Button from './Button';
 import { LARGE, DUSK } from '../Utils/constant';
 import AddBoxIcon from '@material-ui/icons/AddBox';
+import SimpleStorageContract from '../contracts/SimpleStorage.json';
+import getWeb3 from '../getWeb3';
 
 export default class CreateNewGroupModal extends Component {
 	state = {
+		web3: null, 
+		accounts: null, 
+		contract: null, 
 		inputArray: [ '' ],
 		input: null,
-		group: null
+		group: null,
+		groupNumber: null
 	};
-	handleOnClick = () => {
+
+	componentDidMount = async () => {
+		try {
+			// Get network provider and web3 instance.
+			const web3 = await getWeb3();
+
+			// Use web3 to get the user's accounts.
+			const accounts = await web3.eth.getAccounts();
+
+			// Get the contract instance.
+			const networkId = await web3.eth.net.getId();
+			const deployedNetwork = SimpleStorageContract.networks[networkId];
+			const instance = new web3.eth.Contract(
+				SimpleStorageContract.abi,
+				deployedNetwork && deployedNetwork.address
+			);
+
+			instance.events.LogSet((error, event) => {
+				console.log(event);
+			});
+
+			// Set web3, accounts, and contract to the state, and then proceed with an
+			// example of interacting with the contract's methods.
+			this.setState({ web3, accounts, contract: instance });
+		} catch (error) {
+			// Catch any errors for any of the above operations.
+			alert(`Failed to load web3, accounts, or contract. Check console for details.`);
+			console.error(error);
+		}
+	};
+
+	createGroup = async (event) => {
+		event.preventDefault()
+		
+		const { accounts, contract } = this.state;
+		const response = await contract.methods.createGroup(this.state.input, this.state.groupNumber).call();
+	  
+		// Update state with the result.
+		this.setState({ groupNumber: response <= this.state.groupNumber ? parseInt(this.state.groupNumber) + 1 : response, groupName: this.state.input });
+	  };
+
+	handleOnClick = async () => {
 		console.log('create group!');
 
-		
-
+		const { accounts, contract } = this.state;
+		for (var i = 0; i < this.state.inputArray.length; i++) {
+			try {
+				const response = await contract.methods.joinGroup(this.state.inputArray[i], this.state.groupNumber).call();
+			} catch (e) {
+				console.log(e);
+				break;
+			}
+		}
 	};
 
 	onTextChange = (address, index) => {
@@ -84,7 +138,7 @@ export default class CreateNewGroupModal extends Component {
 						<AddBoxIcon onClick={this.handleOnAdd} />
 					</DialogContent>
 					<ActionWrapper>
-						<Button width={150} label={'Create'} handleOnClick={this.handleOnClick && closeModal} />
+						<Button width={150} label={'Create'} handleOnClick={this.handleOnClick && this.createGroup && closeModal} />
 					</ActionWrapper>
 				</Dialog>
 			</div>
